d9b75304e543e3d01b76e6d1cdccd7ea59cf189d
kou <e096112@gmail.com>
2025-09-04 12:46:36 +0900

    ライブラリ表記の追加

--- faq/faq.js
+++ faq/faq.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const faqItems = document.querySelectorAll('.faq-item');
 
+    // 質問部分（ボタン）を見つけてくる
     faqItems.forEach(item => {
         const questionButton = item.querySelector('.faq-question');
 
+        // 質問部分がクリックされたら、中の処理を実行する
         questionButton.addEventListener('click', () => {
+            // 必要なパーツを変数に入れておく
             const answer = item.querySelector('.faq-answer');
             const isActive = questionButton.classList.contains('active');
 
